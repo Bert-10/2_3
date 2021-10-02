e31ee60f3f0e5d41ad30f68979ccca95e027f243
Bert-10 <misha.rogrant@mail.ru>
2021-10-03 01:37:13 +0800

    Сделал авто тесты

---
+++ 2_3Tests/LogicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2_3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_3.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CompareTest_1()
+        {
+            string s1 = "рука", s2 = "курица";
+            var message = Logic.Compare(s1, s2);
+            Assert.AreEqual("Да Да Да Да ", message);
+        }
+        [TestMethod()]
+        public void CompareTest_2()
+        {
+            string s1 = "клоп", s2 = "лес";
+            var message = Logic.Compare(s1, s2);
+            Assert.AreEqual("Нет Да Нет Нет ", message);
+
+        }
+        [TestMethod()]
+        public void CompareTest_3()
+        {
+            string s1 = "огр", s2 = "река";
+            var message = Logic.Compare(s1, s2);
+            Assert.AreEqual("Нет Нет Да ", message);
+
+        }
+    }
+}
